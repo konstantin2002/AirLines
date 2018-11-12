f985c38e70f3b107fe5895ad0c9a2b85b212c41a
Konstantin <36499716+konstantin2002@users.noreply.github.com>
2018-11-12 18:43:13 +0200

    Update tables.py

--- tables.py
+++ tables.py
@@ -3,7 +3,8 @@ distances = {'Almaty': {'Kiev': 3500, 'Paris': 5500, 'London': 6000},
              'Paris': {'Almaty': 5500, 'Kiev': 2000, 'London': 500},
              'London': {'Almaty': 6000, 'Kiev': 2500, 'Paris': 500}}
 
-planeModels = {'Boeing737': {'price': 1300000, 'tank_size': 12, 'cnsmptn': 1.5, 'paidPerKm': 90, 'seats': 150},
-               'AirBus325': {'price': 800000, 'tank_size': 10, 'cnsmptn': 1.2, 'paidPerKm': 30, 'seats': 120}}  # cost/tank/cnsmptn/paidPer1000km
+planeModels = {'Boeing737': {'price': 1300000, 'tank_size': 12, 'cnsmptn': 1.5, 'paidPerKm': 40, 'seats': 150},
+               'AirBus325': {'price': 800000, 'tank_size': 10, 'cnsmptn': 1.2, 'paidPerKm': 30, 'seats': 120}, 
+               'Embraer190': {'price': 2000000, 'tank_size': 12, 'cnsmptn': 0.9, 'paidPerKm': 50, 'seats': 120}}  # cost/tank/cnsmptn/paidPer1000km
 
 fuelPrice = 10000  # price per 1 t of fuel
